1314719b7232bcfaa2494fa59fd68960e86d3efa
Eissa Saber <eissapk44@gmail.com>
2021-10-17 04:44:02 +0200

    added helpers

--- src/olum-helpers.js
+++ src/olum-helpers.js
@@ -8,88 +8,60 @@
 (function (root, factory) {
   if (typeof module === "object" && module.exports) module.exports = factory();
   else if (typeof define === "function" && define.amd) define(factory);
-  // else {
-  //   var obj = factory();
-  //   root.$ = obj.$;
-  //   root.debug = obj.debug;
-  //   root.Origin = obj.Origin;
-  //   root.Localize = obj.Localize;
-  //   root.Service = obj.Service;
-  // }
+  else {
+    var obj = factory();
+    for (var key in obj) {
+      root[key] = obj[key];
+    }
+  }
 })(typeof self !== "undefined" ? self : this, function () {
   "use strict";
 
-  /**
-   * helpers for optimizing the code
-   */
-  const global = typeof self !== "undefined" ? self : this;
-  const debugStr = "Olum [warn]:";
-  const quotes = str => "“" + str + "”";
-  const isDev = () => !!["localhost", "127.0.0.1"].includes(location.hostname);
-  const isObj = obj => !!(obj !== null && typeof obj === "object");
-  const isFullArr = arr => !!(isObj(arr) && Array.isArray(arr) && arr.length);
-  const isFullObj = obj => !!(isObj(obj) && Array.isArray(Object.keys(obj)) && Object.keys(obj).length);
-  const isDef = val => !!(val !== undefined && val !== null);
-  const hasProp = (obj, key) => !!obj.hasOwnProperty(key);
+  /* helpers */
+  var global = typeof self !== "undefined" ? self : this;
 
+  function isDef(val) {
+    return (val !== undefined && val !== null);
+  }
+
+  function isDev() {
+    return ["localhost", "127.0.0.1"].indexOf(global.location.hostname) !== -1;
+  }
+
+  function isObj(obj) {
+    return (obj !== null && typeof obj === "object");
+  }
+
+  function isFullArr(arr) {
+    return !!(isObj(arr) && Array.isArray(arr) && arr.length);
+  }
+
+  function isFullObj(obj) {
+    return !!(isObj(obj) && Array.isArray(Object.keys(obj)) && Object.keys(obj).length);
+  }
+
+
+  /* String Methods - uppercase, lowercase, capitalize */
   String.prototype.upper = function () {
     return this.toUpperCase();
   };
   String.prototype.lower = function () {
     return this.toLowerCase();
   };
-
-  /* Capitalize a string */
   String.prototype.cap = function () {
-    return this.toLowerCase()
-      .split(" ")
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+    return this.toLowerCase().split(" ").map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
   };
 
-  /* For debuging purposes, works in dev mode */
-  const debug = (args, level = "log") => {
-    level = level == "err" ? "error" : level;
-    if (isDev()) Array.isArray(args) ? console[level](...args) : console[level](args);
-  };
-
-  /**
-   * A shorthand for selecting nodes from dom
-   *
-   * @example $(".header") // returns 1st element
-   * @example $(".header", true) // returns array
-   * @example $(elm).get("li") // returns 1st li inside elm
-   * @example $(elm).get("li", true) // returns array of li inside elm
-   * @param {String} target can be class, id, tag or Element
-   * @param {Boolean} level (optional) boolean if omited it returns 1st element otherwise (true) returns array
-   */
-  const $ = (target, level, el = document) => {
-    if (typeof target == "string") {
-      const elms = [].slice.call(el.querySelectorAll(target));
-      if (isFullArr(elms)) {
-        if (level) return elms;
-        else return elms[0];
-      } else {
-        if (level) {
-          return [];
-        } else {
-          return null;
-        }
-      }
-    } else {
-      if (!Array.isArray(target) && !!target && target instanceof Element) target.get = (t, l) => $(t, l, target);
-      return target;
-    }
-  };
-
-  /**
-   * A shorthand for addEventListener() - removeEventListener()
-   */
-  function on(event, cb, propagation = false) {
+  /* shortcut for add/remove event listeners */
+  function on(event, cb, propagation) {
+    if (!isDef(propagation)) propagation = false;
     return this.addEventListener(event, cb, propagation);
   }
 
-  function off(event, cb, propagation = false) {
+  function off(event, cb, propagation) {
+    if (!isDef(propagation)) propagation = false;
     return this.removeEventListener(event, cb, propagation);
   }
   Element.prototype.on = on;
@@ -99,35 +71,143 @@
   global.on = on;
   global.off = off;
 
-  /**
-   * Replace string with values
-   *
-   * @example setTemp(`<div>{{name}}</div>`, {name:"olumjs"})
-   */
-  const setTemp = (temp, obj, delimit = ["{{", "}}"]) => {
-    for (let key in obj) {
-      temp = temp.replace(new RegExp(`\\${delimit[0][0]}\\${delimit[0][1]}${key}\\${delimit[1][0]}\\${delimit[1][1]}`, "g"), obj[key]);
-    }
-    return temp;
-  };
-
-  /**
-   * Expand/Collapse element height
-   *
-   * @example elm.toggle(0.6)
-   * @Hint CSS must have `height: 0; overflow: hidden;`
-   */
-  Element.prototype.toggle = function (time = 0.3) {
-    this.style.transition = `height ${time}s ease-in-out`;
+  /* toggle height of elements */
+  Element.prototype.toggle = function (time) {
+    if (!isDef(time)) time = 0.3;
+    this.style.transition = "height " + time + "s ease-in-out";
     if (this.clientHeight == 0) {
       this.style.height = "auto";
-      const h = this.clientHeight + "px";
+      var h = this.clientHeight + "px";
       this.style.height = "0";
-      setTimeout(() => (this.style.height = h), 0);
+      setTimeout(function () {
+        this.style.height = h
+      }, 0);
     } else this.style.height = "0";
   };
 
-  // translations
+  /* works in browser in dev mode */
+  function debug(args, level) {
+    if (!isDef(level)) level = "log";
+    level = level == "err" ? "error" : level;
+    if (isDev()) Array.isArray(args) ? console[level].apply(console, args) : console[level](args);
+  }
+
+  /* nodes selector */
+  function $(target, level, el) {
+    if (!isDef(el)) el = document;
+    if (typeof target == "string") {
+      var elms = [].slice.call(el.querySelectorAll(target));
+      if (isFullArr(elms)) {
+        if (isDef(level) && level === true) return elms;
+        else return elms[0];
+      } else {
+        if (isDef(level)) return [];
+        else return null;
+      }
+    } else {
+      if (!Array.isArray(target) && target && target instanceof Element) {
+        target.get = function (t, l) {
+          return $(t, l, target);
+        }
+      }
+      return target;
+    }
+  }
+
+  /* replace {{stuff}} in a string */
+  function setTemp(temp, obj, delimiters) {
+    if (!isDef(delimiters)) delimiters = ["{{", "}}"];
+    for (var key in obj) {
+      temp = temp.replace(new RegExp("\\" + delimiters[0][0] + "\\" + delimiters[0][1] + key + "\\" + delimiters[1][0] + "\\" + delimiters[1][1], "g"), obj[key]);
+    }
+    return temp;
+  }
+
+  /* A service for sharing data between components */
+  function Service(event) {
+    if (!(this instanceof Service)) throw new Error("can't invoke 'Service' without 'new' keyword");
+    if (!isDef(event)) throw new Error("event name is not defined in 'Service'");
+    this.event = event;
+
+    var e = new CustomEvent(event, {
+      detail: {},
+      bubbles: true,
+      cancelable: true,
+      composed: false,
+    });
+
+    this.trigger = function () {
+      if (isDef(e)) dispatchEvent(e);
+      else throw new Error("event object is not defined");
+    }
+  }
+
+
+  /**
+   * @example 
+   * origin.method(url, {
+    body: {
+      name: "olumjs"
+    },
+    headers:{
+      "Content-Type": "application/json"
+    }
+  }).then(console.log).catch(console.error)
+   */
+
+  class Origin {
+    constructor() {
+      if (!(this instanceof Origin)) console.error("can't invoke 'Origin' without 'new' keyword");
+      this.xhr = new XMLHttpRequest();
+    }
+
+    setParams(data) {
+      if (isFullObj(data)) {
+        if (data.hasOwnProperty("headers") && isFullObj(data.headers)) {
+          for (let key in data.headers) {
+            this.xhr.setRequestHeader(key, data.headers[key]);
+          }
+        }
+        if (data.hasOwnProperty("body") && isFullObj(data.body)) {
+          return JSON.stringify(data.body);
+        }
+        return null;
+      }
+    }
+
+    onload(resolve, reject) {
+      return (this.xhr.onreadystatechange = () => {
+        if (this.xhr.readyState === 4) {
+          if (this.xhr.status === 0 || (this.xhr.status >= 200 && this.xhr.status <= 299)) {
+            try {
+              resolve(JSON.parse(this.xhr.responseText));
+            } catch (err) {
+              resolve(this.xhr.responseText);
+            }
+          } else if (this.xhr.status >= 400 && this.xhr.status <= 599) {
+            reject(`couldn't reach the server`);
+          }
+        }
+      });
+    }
+
+    req(method, url, data = {}) {
+      return new Promise((resolve, reject) => {
+        this.xhr.open(method, url, true);
+        const form = this.setParams(data);
+        this.onload(resolve, reject);
+        this.xhr.onerror = () => reject(`network error`);
+        form !== null ? this.xhr.send(form) : this.xhr.send();
+      });
+    }
+
+    get = (url, data = {}) => this.req("GET", url, data);
+    post = (url, data = {}) => this.req("POST", url, data);
+    delete = (url, data = {}) => this.req("DELETE", url, data);
+    put = (url, data = {}) => this.req("PUT", url, data);
+    patch = (url, data = {}) => this.req("PATCH", url, data);
+  }
+
   class Localize {
     key = "tolang";
 
@@ -195,93 +275,12 @@
     }
   }
 
-  /**
-   * @example origin.method(url, { body: {name:"olumjs"},"Content-Type": "application/json" }).then(console.log).catch(console.error)
-   */
-  class Origin {
-    constructor() {
-      if (!(this instanceof Origin)) console.error(`${debugStr} can't invoke ${quotes("Origin constructor")} without new keyword`);
-      this.xhr = new XMLHttpRequest();
-    }
-
-    setParams(data) {
-      if (isFullObj(data)) {
-        for (let key in data) {
-          if (key !== "body") this.xhr.setRequestHeader(key, data[key]);
-        }
-        if (hasProp(data, "body") && isFullObj(data.body)) {
-          return JSON.stringify(data.body);
-        }
-        return null;
-      }
-    }
-
-    onload(resolve, reject) {
-      return (this.xhr.onreadystatechange = () => {
-        if (this.xhr.readyState === 4) {
-          if (this.xhr.status === 0 || (this.xhr.status >= 200 && this.xhr.status <= 299)) {
-            try {
-              resolve(JSON.parse(this.xhr.responseText));
-            } catch (err) {
-              resolve(this.xhr.responseText);
-            }
-          } else if (this.xhr.status >= 400 && this.xhr.status <= 599) {
-            reject(`${debugStr} couldn't reach the server`);
-          }
-        }
-      });
-    }
-
-    req(method, url, data = {}) {
-      return new Promise((resolve, reject) => {
-        this.xhr.open(method, url, true);
-        const form = this.setParams(data);
-        this.onload(resolve, reject);
-        this.xhr.onerror = () => reject(`${debugStr} network error`);
-        form !== null ? this.xhr.send(form) : this.xhr.send();
-      });
-    }
-
-    get = (url, data = {}) => this.req("GET", url, data);
-    post = (url, data = {}) => this.req("POST", url, data);
-    delete = (url, data = {}) => this.req("DELETE", url, data);
-    put = (url, data = {}) => this.req("PUT", url, data);
-    patch = (url, data = {}) => this.req("PATCH", url, data);
-  }
-
-  /**
-   * @example const event = new Service("eventName");
-   * window.on(event.event, () => console.log("fired"));
-   * event.trigger()
-   */
-  class Service {
-    constructor(event) {
-      if (!(this instanceof Service)) console.error(`${debugStr} can't invoke ${quotes("Service constructor")} without new keyword`);
-      this.event = event;
-      this.init();
-      return this;
-    }
-
-    init() {
-      this.serviceEvent = new CustomEvent(this.event, {
-        detail: {},
-        bubbles: true,
-        cancelable: true,
-        composed: false,
-      });
-    }
-
-    trigger() {
-      if (isDef(this.serviceEvent)) dispatchEvent(this.serviceEvent);
-    }
-  }
-
   return {
-    Service,
-    Origin,
     $,
     debug,
     setTemp,
+    Service,
+    Origin,
     Localize,
   };
 });
